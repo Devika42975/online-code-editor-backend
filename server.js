786891bd3a63ba8d4d260d421fd3da284dfc8780
Devika42975 <your-email@example.com>
2025-07-13 19:08:52 +0530

    final working backend

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.use(express.json());
 // 👇 This matches frontend axios URL: http://localhost:5000/execute
 app.use("/execute", runRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
